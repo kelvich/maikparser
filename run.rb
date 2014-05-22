58db93bb3919b0c771d79bd21a1aaf08848236cf
Stas Kelvich <stanconn@gmail.com>
2014-05-22 18:11:53 +0400

    не вылетать на ошибке

--- run.rb
+++ run.rb
@@ -7,17 +7,19 @@ require './parser.rb'
 # увеличил article_ref.name
 # добавил doi
 # увеличил кол-во цитирований, встретилось 131
-
-
+# увеличил текст в цитированиях
 
 # parse_issues
 
-
-maik_ids = Article.pluck(:maik_id)[141..-1]
+maik_ids = Article.pluck(:maik_id)[257..-1]
 maik_ids.each_with_index do |maik_id,i|
-  puts "Parsing ##{maik_id} (#{i+1}/#{maik_ids.count})."
-  parse_article(maik_id)
-  puts "  Parsed ##{maik_id} (#{i+1}/#{maik_ids.count})."
+  begin
+    puts "Parsing ##{maik_id} (#{i+1}/#{maik_ids.count})."
+    parse_article(maik_id)
+    puts "  Parsed ##{maik_id}"
+  rescue
+    puts "  !Failed ##{maik_id}"
+  end
 end
 
 
