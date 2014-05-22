6b73de6a02a7144252f72f796d3429f8406cd8b8
Stas Kelvich <stanconn@gmail.com>
2014-05-22 18:19:21 +0400

    bundler

--- run.rb
+++ run.rb
@@ -1,15 +1,16 @@
+require 'bundler/setup'
+
 require './models.rb'
 require './parser.rb'
 
 # добавил maik_id
 # Issue.number лучше тектовым, из-за двойных
-# not nulls
-# увеличил article_ref.name
+# убрал not nulls
+# увеличил article_references.text, встречаются жирные
 # добавил doi
 # увеличил кол-во цитирований, встретилось 131
-# увеличил текст в цитированиях
 
-# parse_issues
+parse_issues
 
 maik_ids = Article.pluck(:maik_id)[257..-1]
 maik_ids.each_with_index do |maik_id,i|
